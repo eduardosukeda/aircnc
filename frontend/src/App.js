27ce2b194146fc5a789790b7e13103cf5103cdda
eduardosukeda <eduardosukeda@live.com>
2019-10-21 16:41:36 -0300

    Finalizando front-end

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,20 +2,20 @@ import React from 'react';
 
 import './App.css';
 
-import logo from './assets/logo.svg'
+import logo from './assets/logo.svg';
 
-import Routes from './routes'
+import Routes from './routes';
 
 function App() {
+  return (
+    <div className="container">
+      <img src={logo} alt="AirCnC" />
 
-    return (
-        <div className="container">[
-            <img src={logo} alt="AirCnc" />
-            <div className="content">
-                <Routes></Routes>
-            </div>
-        </div>
-    );
+      <div className="content">
+        <Routes />
+      </div>
+    </div>
+  );
 }
 
 export default App;
